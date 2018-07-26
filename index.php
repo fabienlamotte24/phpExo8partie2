e045bc7f042e34a65772a513dbc337a77a3bd60f
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 09:43:47 +0200

    exo8p2 php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <p>
+<?php
+  $isOk = false;
+  if($isOk){
+    echo 'C\'est ok !!';
+  } else {
+    echo 'c\'est pas bon !!!';
+  }
+?>
+    </p>
+  </body>
+</html>
